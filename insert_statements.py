cb370e60362efcda788f216788d3ef073c2d8225
wpdoughe <wpdoughe@buffalo.edu>
2022-04-01 12:37:26 -0400

    added seasons data

---
+++ insert_statements.py
@@ -0,0 +1,2 @@
+def seasons(year, gp, w, l, otl, sol, pts, mp):
+    return f"INSERT INTO Seasons VALUES({year}, {gp}, {w}, {l}, {otl}, {sol}, {pts}, {mp})"
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import db_connect
 import create_tables
+import insert_statements
+import csv
 
 #connect
 conn = db_connect.db_connect()
@@ -30,5 +32,18 @@ def create_all_tables():
 #Function ran and tables were created on 3/30/22
 #create_all_tables()
 
+def insert_all_seasons_data():
+    path = "C:\\Users\\Work\\OneDrive\\Desktop\\CSE 460 Project\\csv_files\\seasons_csv.csv"
+    with open(path) as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            mp = b'0'
+            if row[7] == 'Yes':
+                mp = b'1'
 
+            cur.execute(insert_statements.seasons(row[0], row[1], row[2], row[3], row[4], row[5], row[6], mp))
+            conn.commit()
+            
+#Function ran and seasons data added on 4/1
+#insert_all_seasons_data()
 conn.close()
